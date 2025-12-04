2e038e9f6cfa17e5f6532fbe1fd6150b47c2ad51
AlexxSotelo <alexxsotelop@gmail.com>
2025-12-04 17:19:33 -0500

    Se han añadido una página nueva de ofertas.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*botón*/ 
 const jobsListingSection = document.querySelector('.job-listings');
 
 jobsListingSection?.addEventListener('click', (event) => {
@@ -10,3 +11,34 @@ jobsListingSection?.addEventListener('click', (event) => {
     }
 });
         
+/*filtros de busqueda*/ 
+const filtro = document.querySelector('#location');
+const mensaje = document.querySelector('#filter-selected-value');
+
+filtro?.addEventListener('click', (event) => {
+    const selectedLocation = filtro.value;
+
+    if (selectedTech) {
+        mensaje.textContent = `Has seleccionado: ${selectedLocation}`;
+    } else {
+        mensaje.textContent = '';
+    }
+});
+
+/*barra de busqueda*/
+const searchInput = document.querySelector('#empleos-search-input');
+
+searchInput?.addEventListener('input', (event) => {
+    console.log(searchInput.value);
+});
+
+searchInput?.addEventListener('blur', (event) => {
+    console.log('se perdió el foco');
+});
+
+const searchForm = document.querySelector('#empleos-search-form');
+
+searchForm?.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Formulario enviado');
+});
